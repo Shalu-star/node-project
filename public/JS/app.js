e358dfbd5345812354d310f80f3332a613e8db23
Shalu-star <shalukris7@gmail.com>
2021-09-27 11:53:34 +0530

    Commenting console.log

--- public/JS/app.js
+++ public/JS/app.js
@@ -1,4 +1,4 @@
-console.log('Client Side JS is loaded')
+//console.log('Client Side JS is loaded')
 
 // fetch('http://puzzle.mead.io/puzzle').then((response) => {
 //     response.json().then((data) => {
